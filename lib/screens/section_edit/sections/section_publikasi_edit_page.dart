c747c4a7bf32ebcbd5e7cfbbcfb7a809b28323fa
Owen Susanto <owen@susanto.me>
2023-03-30 21:21:33 +0700

    fix some view

--- lib/screens/section_edit/sections/section_publikasi_edit_page.dart
+++ lib/screens/section_edit/sections/section_publikasi_edit_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_phoenix/models/sections/publikasi_section.dart';
 import 'package:flutter_phoenix/screens/section_edit/section_edit_page.dart';
 import 'package:flutter_phoenix/widgets/base_raised_button.dart';
 import 'package:flutter_phoenix/widgets/custom/custom_text.dart';
+import 'package:flutter_phoenix/widgets/date_time_picker_form_field.dart';
 import 'package:flutter_phoenix/widgets/normal_form_field.dart';
 import 'package:provider/provider.dart';
 
@@ -15,28 +16,83 @@ class SectionPublikasiEditPage extends SectionEditPage {
   @override
   Widget formView(BuildContext context) {
     FocusNode _nameFocusNode = FocusNode();
+    FocusNode _tanggalFocusNode = FocusNode();
+    FocusNode _topikFocusNode = FocusNode();
+    FocusNode _bidangIlmuFocusNode = FocusNode();
+    FocusNode _descriptionFocusNode = FocusNode();
     return Consumer<PublikasiSection>(builder: (_, publikasi, __) {
       return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 5),
           const CustomText(
-            'Nama Lengkap',
+            'Nama',
             fontSize: 15,
             fontWeight: FontWeight.bold,
           ),
           NormalFormField(
-            hintText: "ex. Budi Gunawan",
-            text: publikasi.name ?? "",
+            hintText: "ex. Bachelor of Science",
             focusNode: _nameFocusNode,
             onFieldSubmitted: (value) {
               _nameFocusNode.unfocus();
-              // FocusScope.of(context).requestFocus(_emailFocusNode);
+              FocusScope.of(context).requestFocus(_tanggalFocusNode);
             },
-            // onChanged: (value) {
-            //   _registerData.name = value;
-            // },
-            // validator: (value) => pelatihan!.nameValidator(),
+            onChanged: (value) {},
+          ),
+          const SizedBox(height: 10),
+          const CustomText(
+            'Tanggal Berakhir Pelatihan',
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+          DateTimePickerFormField(
+            focusNode: _tanggalFocusNode,
+            onChanged: (val) {},
+          ),
+          const SizedBox(height: 10),
+          const CustomText(
+            'Bidang Ilmu',
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+          NormalFormField(
+            hintText: "ex. Biologi",
+            focusNode: _bidangIlmuFocusNode,
+            onFieldSubmitted: (value) {
+              _bidangIlmuFocusNode.unfocus();
+              FocusScope.of(context).requestFocus(_topikFocusNode);
+            },
+            onChanged: (value) {},
+          ),
+          const SizedBox(height: 10),
+          const CustomText(
+            'Topik',
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+          NormalFormField(
+            hintText: "ex. MikroBiologi",
+            focusNode: _topikFocusNode,
+            onFieldSubmitted: (value) {
+              _topikFocusNode.unfocus();
+              FocusScope.of(context).requestFocus(_topikFocusNode);
+            },
+            onChanged: (value) {},
+          ),
+          const SizedBox(height: 10),
+          const CustomText(
+            'Deskripsi',
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+          NormalFormField(
+            hintText: "ex. Deskripsi",
+            focusNode: _descriptionFocusNode,
+            onFieldSubmitted: (value) {
+              _descriptionFocusNode.unfocus();
+              FocusScope.of(context).requestFocus(_descriptionFocusNode);
+            },
+            onChanged: (value) {},
           ),
           const SizedBox(height: 45),
           Center(
